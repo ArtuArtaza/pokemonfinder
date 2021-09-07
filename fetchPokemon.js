e609cbc657598ce47792f2944770fb4256b700d8
ArtuArtaza <71021164+ArtuArtaza@users.noreply.github.com>
2021-09-07 17:05:16 -0300

    Add files via upload

---
+++ fetchPokemon.js
@@ -0,0 +1,18 @@
+import setState,{State} from "./setState.js"
+
+const fetchPokemon = (url) => {
+    const loadText = document.getElementById('load')
+    loadText.innerText = 'Cargando data...'
+
+    fetch(url).
+    then(res => res.json()).
+    then(data => {
+        setState({pokemonList:data.results})
+        loadText.innerText = ''
+        }
+        )
+        
+}
+
+export default fetchPokemon
+
